e0549ca40fc858c287b52133315b042341b95187
tutumao-5 <jinweixi16@gmail.com>
2026-02-20 19:13:28 +0800

    Create callback.js

---
+++ functions/api/callback.js
@@ -0,0 +1,47 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const code = url.searchParams.get('code');
+  const client_id = context.env.GITHUB_CLIENT_ID;
+  const client_secret = context.env.GITHUB_CLIENT_SECRET;
+
+  try {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id,
+        client_secret,
+        code,
+      }),
+    });
+
+    const data = await response.json();
+    const token = data.access_token;
+
+    if (!token) throw new Error('授权失败，未获取到 token');
+
+    const script = `
+      <script>
+        const receiveMessage = (message) => {
+          if (message.data === "authorizing:github") {
+            window.opener.postMessage(
+              'authorization:github:success:{"token":"${token}","provider":"github"}',
+              message.origin
+            );
+            window.removeEventListener("message", receiveMessage, false);
+          }
+        }
+        window.addEventListener("message", receiveMessage, false);
+        window.opener.postMessage("authorizing:github", "*");
+      </script>
+    `;
+    return new Response(script, {
+      headers: { 'Content-Type': 'text/html;charset=UTF-8' },
+    });
+  } catch (error) {
+    return new Response(error.message, { status: 500 });
+  }
+}
