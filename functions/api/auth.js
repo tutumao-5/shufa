3acf68946fbf0271e9796ea84f3251501ec751cc
tutumao-5 <jinweixi16@gmail.com>
2026-02-20 20:13:51 +0800

    Delete functions/api directory

--- functions/api/auth.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const client_id = context.env.GITHUB_CLIENT_ID;
-  const url = new URL('https://github.com/login/oauth/authorize');
-  url.searchParams.set('client_id', client_id);
-  url.searchParams.set('scope', 'repo,user');
-  return Response.redirect(url.toString(), 302);
-}
